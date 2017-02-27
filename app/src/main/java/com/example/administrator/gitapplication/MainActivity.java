a7947f1e4c5fe1667ef1bcec4c3db48a61a8f77a
Lxy <1846458698@qq.com>
2017-02-27 17:13:45 +0800

    第4次提交

--- app/src/main/java/com/example/administrator/gitapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/gitapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("hhhh","hhhh");
-        Toast.makeText(getApplicationContext(),"hhhhh",Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(),"hhhhh",Toast.LENGTH_LONG).show();
     }
 }
